2787f437e87e08780cd4cc1f531011cfe692545b
Brendan Boyle <brendancboyle@me.com>
2013-10-01 15:15:02 -0500

    Changed logout button styling

--- M9/Admin.php
+++ M9/Admin.php
@@ -10,6 +10,7 @@
     <body onload="Core.load();">
         <div id="nav">
             <img alt="Profile Pic" src="http://www.gravatar.com/avatar/<?php echo user::getGravatar(); ?>?s=400" id="profilepic" />
+            <input type="button" value="Logout" class="btn btn-primary" onclick="window.location = '/M9/Logout.php'" />
         </div>
         <div id="content">
             <h1>M9 Admin Panel</h1>
@@ -17,7 +18,6 @@
                 cards::adminPanel();
             ?>
             <hr />
-            <input type="button" value="Logout" onclick="window.location = '/M9/Logout.php'" />
         </div>
     </body>
 </html>
