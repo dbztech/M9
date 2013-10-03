669a9bbb5e8f172e14ef2b61b18dc7b3438f860f
Brendan Boyle <brendancboyle@me.com>
2013-10-03 12:55:24 -0500

    Updated Login Page

--- M9/Login.php
+++ M9/Login.php
@@ -1,16 +1,20 @@
 <!doctype html>
 <html>
     <head>
+        <link href="/M9/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link rel="stylesheet" href="styles.css" />
+        <script src="/M9/bootstrap/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="scripts.js"></script>
     </head>
-    <body>
-        <h1>M9 Admin Panel</h1>
-        <h2>Login:</h2>
-        <form action="/M9/" method="post">
-            <input type="email" name="username" placeholder="Email" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <input type="submit" />
-        </form>
+    <body style="background-color: #eee;">
+        <div class="hero-unit">
+            <h1>M9 Admin Panel</h1>
+            <h2>Login:</h2>
+            <form action="/M9/" method="post">
+                <input type="email" name="username" placeholder="Email" required /><br />
+                <input type="password" name="password" placeholder="Password" required /><br />
+                <input type="submit" class="btn btn-primary" />
+            </form>
+        </div>
     </body>
 </html>
