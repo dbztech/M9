07fa19622bfe42356adfbc80158c50ef7bb99216
Brendan Boyle <brendancboyle@me.com>
2013-11-04 12:33:45 -0600

    Returned to default config

--- M9/config.php
+++ M9/config.php
@@ -1,5 +1,5 @@
 <?php
 #Database configuration
 #User, Password, Host, Database, Prefix (Not Used)
-database::setcredentials("M9", "hunter3", '127.0.0.1', 'testm9');
+database::setcredentials("M9", "hunter3", '127.0.0.1', 'm9');
 ?>
