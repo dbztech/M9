5207b1d6e98ab3fe2281dc2399e3e387d6c64c00
Michael Wilke <mwilke16@moundsparkacademy.org>
2013-12-30 13:11:00 -0600

    Fixes apache errors with finding files

--- M9/M9.php
+++ M9/M9.php
@@ -1,8 +1,8 @@
 <?php
 #M9 Interaction File
 if (file_exists($_SERVER['DOCUMENT_ROOT'].'/M9/config.php')) {
-    include('classes.php');
-    include('config.php');
+    include('M9/classes.php');
+    include('M9/config.php');
 } else {
     header('Location: /M9/Setup/');
 }
