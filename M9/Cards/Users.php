0224113ae0bbd697ff8b30bffa24c913004c9773
Brendan Boyle <brendancboyle@me.com>
2014-12-03 22:50:00 -0500

    Made changes to generic pages and hash tool

--- M9/Cards/Users.php
+++ M9/Cards/Users.php
@@ -1,4 +1,4 @@
-<h1 class="lead" style="margin-bottom: 0px;">User Managment</h1>
+<h1 class="lead" style="margin-bottom: 0px;">User Management</h1>
 
 <a href="#Users" onclick="User.change();"><h2 style="background: #FFFFFF url('Resources/Images/19.jpg');" class="filltext">Manage Users</h2></a>
 <a href="#CreateUsers" onclick="User.create();"><h2 style="background: #FFFFFF url('Resources/Images/9.jpg');" class="filltext">Create User</h2></a>
--- M9/Login.php
+++ M9/Login.php
@@ -1,16 +1,7 @@
 <!doctype html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/M9/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link rel="stylesheet" href="styles.css" />
-        <script src="/M9/bootstrap/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="scripts.js"></script>
-        <link rel="shortcut icon" href="/M9/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="/M9/favicon.ico" type="image/x-icon">
-        <title>M9 Login</title>
-    </head>
-    <body style="background-color: #eee;">
+    <?php  $title = "M9 Login"; include('GenericHead.php'); ?>
+    <body>
         <div class="jumbotron">
             <h1>M9 Admin Panel</h1>
             <h2>Login:</h2>
