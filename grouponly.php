8a7b9cf0ce2e27f15cc502c5c4e0a3f6b94fe781
Brendan Boyle <brendancboyle@me.com>
2015-03-25 13:58:56 -0400

    Because I can

--- grouponly.php
+++ grouponly.php
@@ -4,7 +4,7 @@
     <head></head>
     <body>
         <h1>Welcome to M9!</h1>
-        <h2>You are a special snowflake</h2>
+        <h2>You are a special snowflakee</h2>
         <?php M9::data('trialcontent'); ?>
     </body>
 </html>
