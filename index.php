cfb75dd089a0e5ba0fe1c98e95b21bddd962055a
Brendan Boyle <brendan@dbztech.com>
2015-10-06 23:09:47 -0400

    10/6 Update

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<?php include('M9/M9.php'); M9::devmode('index2.php'); M9::start(); ?>
+<?php include('M9/M9.php'); M9::start(); ?>
 <!doctype html>
 <html>
     <head></head>
     <body>
-        <h1>Welcome to M9!</h1>
-        <h2>Put your website here</h2>
-        <?php M9::data('demotitle'); ?>
+        <h1>Welcome to HERO Capture</h1>
+        <h2>Copyright 2015 Human Element Robotics</h2>
     </body>
 </html>
-<?php M9::loadtime(); ?>
